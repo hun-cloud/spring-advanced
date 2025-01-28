8ecffe061ee23263fc6647221b94f21f062becd6
JaeHunJaeHun <jhl9838@gmail.com>
2025-01-28 19:09:49 +0900

    feat: 로그추적기 v1 프로토타입 개발

---
+++ src/test/java/hello/advanced/trace/hellotrace/HelloTraceV1Test.java
@@ -0,0 +1,26 @@
+package hello.advanced.trace.hellotrace;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloTraceV1Test {
+
+    @DisplayName("begin_end")
+    @Test
+    void begin_end() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status =  trace.begin("hello");
+        trace.end(status);
+    }
+
+    @DisplayName("begin_exception")
+    @Test
+    void begin_exception() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status =  trace.begin("hello");
+        trace.exception(status, new IllegalStateException());
+    }
+}
