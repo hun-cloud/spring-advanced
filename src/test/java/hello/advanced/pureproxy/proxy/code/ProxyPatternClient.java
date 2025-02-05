2683dce4ea2ac2743217cf53e11e928082200f2e
leejaehun <ljh0901@mlsoft.com>
2025-02-05 11:15:57 +0900

    프록시 패턴 - 예제 코드1

---
+++ src/test/java/hello/advanced/pureproxy/proxy/code/ProxyPatternClient.java
@@ -0,0 +1,13 @@
+package hello.advanced.pureproxy.proxy.code;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ProxyPatternClient {
+
+    private final Subject subject;
+
+    public void execute() {
+        subject.operation();
+    }
+}
