3e24b6e98e8bef12ce89c723b8b014f7241fd290
leejaehun <ljh0901@mlsoft.com>
2025-02-05 11:41:31 +0900

    데코레이터 패턴 - 예제코드1

---
+++ src/test/java/hello/advanced/decorator/DecoratorPatternTest.java
@@ -0,0 +1,18 @@
+package hello.advanced.decorator;
+
+import hello.advanced.decorator.code.Component;
+import hello.advanced.decorator.code.DecoratorPatternClient;
+import hello.advanced.decorator.code.RealComponent;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class DecoratorPatternTest {
+
+    @Test
+    void noDecorator() {
+        Component realComponent = new RealComponent();
+        DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+    }
+}
