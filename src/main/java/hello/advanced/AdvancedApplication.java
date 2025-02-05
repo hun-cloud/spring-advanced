61e45ff1052053f29a28e682de8064262815f61f
leejaehun <ljh0901@mlsoft.com>
2025-02-05 13:41:09 +0900

    인터페이스 기반 프록시 - 적용

--- src/main/java/hello/advanced/AdvancedApplication.java
+++ src/main/java/hello/advanced/AdvancedApplication.java
@@ -2,11 +2,16 @@ package hello.advanced;
 
 import hello.advanced.app.proxy.config.AppV1Config;
 import hello.advanced.app.proxy.config.AppV2Config;
+import hello.advanced.app.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV2Config.class)
+@Import(AppV1Config.class)
+// @Import(InterfaceProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.advanced.app.proxy")
 public class AdvancedApplication {
 
@@ -14,4 +19,8 @@ public class AdvancedApplication {
 		SpringApplication.run(AdvancedApplication.class, args);
 	}
 
+	@Bean
+	public LogTrace logTrace() {
+		return new ThreadLocalLogTrace();
+	}
 }
